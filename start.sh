e7334c427d8db536d7af968bd3899bb8a9b0d0b2
Mirshod <python3526@gmail.com>
2025-07-03 20:48:21 +0500

    Refactor start.sh to support both docker-compose and docker commands

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 PORT=$(shuf -i 1025-65535 -n 1)
-PORT=$PORT docker compose up -d --build
+if command -v docker-compose >/dev/null 2>&1; then
+  PORT=$PORT docker-compose up --build -d
+else
+  PORT=$PORT docker compose up --build -d
+fi
 echo "Watermelon website running on port $PORT"
